60b8bef946eabff4a54f9dace0635462443b1b15
ashwani1122 <ashwanisingh3846@gmail.com>
2026-03-16 13:36:26 +0530

    generate p slinet "

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -286,7 +286,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
